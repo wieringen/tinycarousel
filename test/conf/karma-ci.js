5a9b3c53dc32144a73be6d4f8ff3b8ed17e4b2c0
Maarten Baijs <wieringen@gmail.com>
2015-01-19 17:15:57 +0100

    fixed travis builds

--- test/conf/karma-ci.js
+++ test/conf/karma-ci.js
@@ -3,11 +3,23 @@ var ciConfig = require('./karma-common.js');
 module.exports = function (config) {
     ciConfig.browsers = [
         'Firefox',
-        'Chrome'
+        'Chrome',
         //'IE9 - Win7',
         //'IE10 - Win7',
         //'IE11 - Win7'
     ];
+
+    if (process.env.TRAVIS) {
+        ciConfig.customLaunchers = {
+            Chrome_travis_ci: {
+                base: 'Chrome',
+                flags: ['--no-sandbox']
+            }
+        };
+
+        ciConfig.browsers = ['Firefox', 'Chrome_travis_ci'];
+    }
+
     ciConfig.singleRun = true;
     ciConfig.reporters.push('junit');
     ciConfig.junitReporter = {
